494f0ca09e289d417340906ed00865f862be0802
SMRuggiero <AnabelSMRuggiero@gmail.com>
2021-06-11 14:28:24 -0500

    Changed accum in euclidean norm to templated return type. Added Dot product.

--- src/NND/SpaceMetrics.hpp
+++ src/NND/SpaceMetrics.hpp
@@ -7,6 +7,10 @@
 
 namespace nnd{
 
+// I might want to not use valarrays or use template specifications for valarrays. There's some nifty numerical algorithms in the STL
+// that use container semantics, but apparently .begin() and .end() on valarray doesn't support it.
+// /shrug
+
 template<typename DataType, typename RetType=double>
 using SpaceMetric = RetType (*)(const DataType&, const DataType&);
 //using SpaceMetric = std::function<RetType(const std::valarray<DataType>&, const std::valarray<DataType>&)>;
@@ -14,11 +18,21 @@ using SpaceMetric = RetType (*)(const DataType&, const DataType&);
 template<typename DataType, typename RetType=double>
 RetType EuclideanNorm(const std::valarray<DataType>& pointA, const std::valarray<DataType>& pointB){
     std::valarray<DataType> diffs = pointB-pointA;
-    double accum(0);
+    RetType accum(0);
     for(DataType i : diffs){
         accum += i*i;
     }
     return std::sqrt(accum);
+};
+
+template<typename DataType, typename RetType=double>
+RetType Dot(const std::valarray<DataType>& pointA, const std::valarray<DataType>& pointB){
+    std::valarray<DataType> components = pointB*pointA;
+    RetType accum(0);
+    for(DataType i : components){
+        accum += i;
+    }
+    return accum;
 };
 
 
