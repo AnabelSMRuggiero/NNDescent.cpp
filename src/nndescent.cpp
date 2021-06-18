1cf5fd8a1430ac0d6c0c4cdf7ead390cc6bbb664
AnabelSMRuggiero <AnabelSMRuggiero@gmail.com>
2021-06-17 20:43:01 -0700

    Working on procedures to produce some data for visualizing a proof of concept.

--- src/nndescent.cpp
+++ src/nndescent.cpp
@@ -26,7 +26,83 @@ using namespace nnd;
 
 
 
+std::vector<size_t> LabelIndecies(const RandomProjectionForest& forest){
 
+    std::vector<size_t> classifications(forest.indexArray.size());
+
+    std::vector<size_t> treePath;
+    std::vector<char> pathState;
+    treePath.push_back(0);
+    pathState.push_back(0);
+
+    size_t highestIndex = 0;
+
+    const RandomProjectionForest::TreeLeaf* currentLeaf = &(forest.treeLeaves[0]);
+
+
+    while (treePath.size() != 0){
+
+        if(currentLeaf->children.first != nullptr && currentLeaf->children.second != nullptr){
+            if (pathState.back() == 0){
+                pathState.back() = 1;
+                currentLeaf = currentLeaf->children.first;
+                treePath.push_back(currentLeaf->childrenSplittingIndex);
+                pathState.push_back(0);
+                continue;    
+            } else if (pathState.back() == 1) {
+                pathState.back() = 2;
+                currentLeaf = currentLeaf->children.second;
+                treePath.push_back(currentLeaf->childrenSplittingIndex);
+                pathState.push_back(0);
+                continue;
+            } else if (pathState.back() == 2) {
+                currentLeaf = currentLeaf->parent;
+                pathState.pop_back();
+                treePath.pop_back();
+                continue;
+            }
+            throw std::exception("Invalid Crawl State");
+            
+        } else if (currentLeaf->children.first == nullptr && currentLeaf->children.second == nullptr){
+            currentLeaf = currentLeaf->parent;
+            pathState.pop_back();
+            treePath.pop_back();
+            highestIndex = std::max(highestIndex, currentLeaf->childrenSplittingIndex);
+            for (size_t index = currentLeaf->splitRange.first; index < currentLeaf->splitRange.second; index += 1){
+                classifications[forest.indexArray[index]] = currentLeaf->childrenSplittingIndex;
+            }
+            continue;
+        }
+        throw std::exception("Invalid Tree State");
+        //size_t currentIndex = treePath.back();
+
+    }
+
+    return std::move(classifications);
+
+};
+
+std::unordered_map<size_t, std::pair<int, std::valarray<double>>> CalculateCOMs(const MNISTData& digits, const std::vector<size_t>& classifications){
+
+    std::unordered_map<size_t, std::pair<int, std::valarray<double>>> centerOfMasses;
+
+    for (size_t i = 0; i<classifications.size(); i += 1){
+        if (centerOfMasses.find(classifications[i]) == centerOfMasses.end()){
+            centerOfMasses[classifications[i]].first = 1;
+            centerOfMasses[classifications[i]].second = std::valarray<double>(digits.vectorLength);
+            for (size_t j = 0; j<digits.vectorLength; j += 1){
+                centerOfMasses[classifications[i]].second[j] = static_cast<double>(digits.samples[i][j]);
+            }
+        } else {
+            centerOfMasses[classifications[i]].first += 1;
+            for (size_t j = 0; j<digits.vectorLength; j += 1){
+                centerOfMasses[classifications[i]].second[j] += static_cast<double>(digits.samples[i][j]);
+            }
+        }
+    }
+
+    return std::move(centerOfMasses);
+}
 
 
 int main(){
