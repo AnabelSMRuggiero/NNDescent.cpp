b43f3aee488fbb86fc3db66dbaaf767dc9e4018c
AnabelSMRuggiero <AnabelSMRuggiero@gmail.com>
2021-07-01 09:12:47 -0700

    Added a blurb about the current (lack of a) license.

--- src/Utilities/DataDeserialization.hpp
+++ src/Utilities/DataDeserialization.hpp
@@ -1,3 +1,13 @@
+/*
+NNDescent.cpp: Copyright (c) Anabel Ruggiero
+At the time of writting, this code is unreleased and not published under a license.
+As a result, I currently retain all legal rights I am legally entitled to.
+
+I am currently considering a permissive license for releasing this code, such as the Apache 2.0 w/LLVM exception.
+Please refer to the project repo for any updates regarding liscensing.
+https://github.com/AnabelSMRuggiero/NNDescent.cpp
+*/
+
 #ifndef NND_DATADESERIALIZATION_HPP
 #define NND_DATADESERIALIZATION_HPP
 
